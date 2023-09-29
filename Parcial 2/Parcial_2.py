37934b51d7d515a3ffd6b21e8c84d4b971a51a89
Davidsanta0120 <davidsanta2016gmail.com>
2023-09-29 17:28:13 -0500

    si

--- Parcial 2/Parcial_2.py
+++ Parcial 2/Parcial_2.py
@@ -1,8 +1,17 @@
 import numpy as np
 import sympy as sp
 r , w = np.polynomial.legendre.leggauss(20)
-r , w = np.polynomial.legendre.leggauss(50)
+
 h = 6.626 *10**(-34)
 k = 1.3806 * 10**(-23)
 c = 3*10**(8)
 t = 5772
+
+
+
+def Idenominador():
+    r2 , w2 = np.polynomial.legendre.leggauss(20)
+    f2=lambda x: x**3/(1-np.e**(-x))
+    I=np.sum(w2*f(r2))
+    return I
+
