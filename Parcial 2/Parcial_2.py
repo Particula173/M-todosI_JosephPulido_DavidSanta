d3631269fa9eaf0505aae19687642458665427db
Particula173 <je.pulidog1@uniandes.edu.co>
2023-09-29 17:52:00 -0500

    fin

--- Parcial 2/Parcial_2.py
+++ Parcial 2/Parcial_2.py
@@ -16,18 +16,18 @@ def Idenominador():
     I=np.sum(w2*f2(r2))
     return I
 
-landa_0 = 100
-landa_1 = 400
+landa_0 = 100*10**(-9)
+landa_1 = 400*10**(-9)
 v_1 = (h*(c / landa_1))/(k*t)
 v_0 = (h*(c / landa_0))/(k*t)
 g=lambda x : (x**3)/(((np.e)**x)-1)
 def integral_numerador (v_0,v_1):
     r , w = np.polynomial.legendre.leggauss(20)
-    integral =np.sum(w*g((((r*(v_1-v_0))/2)+((v_0+v_1)/2))))  
+    integral =(np.sum(w*g((((r*(v_1-v_0))/2)+((v_0+v_1)/2)))))*((v_1-v_0)/2)  
     return integral
 
 
-num = integral_numerador(v_0,v_1)
+num = integral_numerador(v_1,v_0)
 den = Idenominador()
-frac = num/den
+frac = (num/den)*100
 print(frac)
