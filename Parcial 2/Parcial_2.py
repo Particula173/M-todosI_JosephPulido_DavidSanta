09544d5f0b2120ee2e884b0cc4dddfc1f6da652c
Particula173 <je.pulidog1@uniandes.edu.co>
2023-09-29 17:15:54 -0500

    parcial

---
+++ Parcial 2/Parcial_2.py
@@ -0,0 +1,8 @@
+import numpy as np
+import sympy as sp
+r , w = np.polynomial.legendre.leggauss(20)
+r , w = np.polynomial.legendre.leggauss(50)
+h = 6.626 *10**(-34)
+k = 1.3806 * 10**(-23)
+c = 3*10**(8)
+t = 5772
