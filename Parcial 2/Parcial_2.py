f3b12184e92fb0cb61a7ed170611e6625a243b1a
Particula173 <je.pulidog1@uniandes.edu.co>
2023-09-29 17:34:57 -0500

    a

--- Parcial 2/Parcial_2.py
+++ Parcial 2/Parcial_2.py
@@ -1,8 +1,20 @@
 import numpy as np
 import sympy as sp
 r , w = np.polynomial.legendre.leggauss(20)
-r , w = np.polynomial.legendre.leggauss(50)
+
 h = 6.626 *10**(-34)
 k = 1.3806 * 10**(-23)
 c = 3*10**(8)
 t = 5772
+landa_0 = 100*10**(-9)
+landa_1 = 400*10**(-9)
+v_1 = (h*(c / landa_1))/(k*t)
+v_0 = (h*(c / landa_0))/(k*t)
+g=lambda x : (x**3)/(((np.e)**x)-1)
+def integral_numerador (v_0,v_1):
+    r , w = np.polynomial.legendre.leggauss(20)
+    integral =np.sum(w*g((((r*(v_1-v_0))/2)+((v_0+v_1)/2))))  
+    return integral
+print(integral_numerador(v_0,v_1))
+x = sp.symbols("x")
+verificacion = sp.integrate(g(x), x,)
