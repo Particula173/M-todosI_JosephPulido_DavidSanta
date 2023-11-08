b4e79e905732fc408b930cb24a9ecd88fc35728f
Davidsanta0120 <davidsanta2016gmail.com>
2023-11-08 17:41:32 -0500

    parcial computacional

--- Parcial 3/parcial parte practica.py
+++ Parcial 3/parcial parte practica.py
@@ -6,12 +6,15 @@ def conjugado (A,b,x_k,e=0.01):
     r_k = np.dot(A,x_k)-b
     p_k = -r_k
     k = 0
-    while np.max(np.abs(r_k)) > e:
-        al_k = -((np.dot(r_k.T,p_k))/(p_k.T*np.dot(A,p_k)))
-        x_k = x_k + al_k*p_k
+    while np.max(np.abs(r_k)) > e :
+        al_k = -((np.dot(r_k.T,p_k))/(np.dot(p_k.T,np.dot(A,p_k))))
+        x_k = x_k + np.dot(al_k,p_k)
         r_k = np.dot(A,x_k)-b
-        beta_k = r_k.T*np.dot(A,p_k) / p_k.T*np.dot(A,p_k)
+        beta_k = np.dot(r_k.T,np.dot(A,p_k)) / np.dot(p_k.T,np.dot(A,p_k))
         p_k = -r_k+np.dot(beta_k,p_k)
         k += 1
+    print(k)
     return x_k
 print(conjugado(M,b,np.array([1,1,1,1,1])))
+
+print(np.linalg.solve(M,b))
